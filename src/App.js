2374fba97a0005e27895717ddad58476f99309f0
kangkan <kangkanjr@gmail.com>
2021-07-02 12:19:47 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hardest to get more minimal than this React app.</p>
+    <p>Hello! This is your boy KJ!</p>
+  </>
+  );
+};
+
+export default App;
